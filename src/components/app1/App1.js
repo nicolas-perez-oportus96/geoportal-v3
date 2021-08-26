9008506584ded34ad9e9ef3128bceb8baabf6228
Nicolás Pérez Oportus <nicolas.perez.oportus96@gmail.com>
2021-08-26 07:27:26 -0400

    Titulo app1

--- src/components/app1/App1.js
+++ src/components/app1/App1.js
@@ -12,7 +12,7 @@ export default class App1 extends Component {
                     <div className="container flex">
                         <img src="/assets/app-icon.svg" alt="app-icon" />
                         <div className="texto">
-                            <h1>Aplicacion 1: <span>Glaciar Hielo</span></h1>
+                            <h1>Aplicacion 1: <span>Inventario Nacional de Glaciares de Chile Central</span></h1>
                             <p>Haz clic sobre un polígono para seleccionarlo y ver sus metadatos.</p>
                         </div>
                     </div>
