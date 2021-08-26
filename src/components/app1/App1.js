72aea1149bcfce3a6d963bf254f9d0767a3d4da3
Nicolás Pérez Oportus <nicolas.perez.oportus96@gmail.com>
2021-08-26 07:21:49 -0400

    ruta icono app

--- src/components/app1/App1.js
+++ src/components/app1/App1.js
@@ -10,7 +10,7 @@ export default class App1 extends Component {
             <main className="app-container">
                 <div className="instrucciones">
                     <div className="container flex">
-                        <img src="/assets/app.svg" alt="app-icon" />
+                        <img src="/assets/app-icon.svg" alt="app-icon" />
                         <div className="texto">
                             <h1>Aplicacion 1: <span>Glaciar Hielo</span></h1>
                             <p>Haz clic sobre un polígono para seleccionarlo y ver sus metadatos.</p>
